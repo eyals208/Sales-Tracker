f75c303d6d2741393b469934ad3b9e09367a7eca
eyals208 <eyalsrugo@gmail.com>
2024-08-12 17:47:20 +0300

    start pages foundation

--- sales_tracker/routes.py
+++ sales_tracker/routes.py
@@ -6,3 +6,7 @@ pages = Blueprint("pages", __name__, template_folder= "templates", static_folder
 @pages.route("/")
 def home():
     return render_template("home.html")
+
+@pages.route("/register")
+def register():
+    return render_template("register.html")
